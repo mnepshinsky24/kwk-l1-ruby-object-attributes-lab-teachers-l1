b9a2bd47e44e6aa7ed80101e004aca4e2ffc47e1
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-06-24 22:23:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+
+class Dog
+
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed=(breeds)
+    @breed = breeds
+  end 
+  
+  def breed
+    @breed
+  end 
+  
+end 
